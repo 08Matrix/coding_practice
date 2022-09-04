8c86cb83f89aebafd76b932a720cd3114c864bb3
rajdeep-paul16 <rajdeeppaul1614@gmail.com>
2022-09-04 19:39:17 +0530

    modified the attendence program

--- files/attendence.java
+++ files/attendence.java
@@ -13,12 +13,21 @@ public class attendence {
         float count = (m / n) * 100;
 
         if (count > 75) {
-            System.out.println("Your attendence is = " + count );
+            System.out.println("Your attendence is = " + count + "%" );
             System.out.println("Your allowed to sit in examination");}
 
         else {
-            System.out.println("Your attendence is = " + count );
-            System.out.println("Your not allowed to sit in examination");}
+            System.out.println("Your attendence is = " + count + "%" );
+            System.out.println("Your not allowed to sit in examination due to less attendence");
+
+            System.out.println("Do you have any medical cause, if yes type 'y' or 'n' for no");
+            char yn = input.next().charAt(0);
+
+            if (yn == 'y') {
+            System.out.println("Your allowed to sit in examination because of medical cause");}
+
+            else {    
+            System.out.println("You don't have any medical cause. Your not allowed to sit in examination due to less attendence");}}
             
         input.close();    
 
