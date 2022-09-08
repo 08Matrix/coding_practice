cd973f1dd4e68bd462ee05da3b6cb2aae1afc972
rajdeep-paul16 <rajdeeppaul1614@gmail.com>
2022-09-08 15:02:32 +0530

    corrected all the mistakes

--- files/diamondNo.java
+++ files/diamondNo.java
@@ -7,7 +7,7 @@ public class diamondNo {
         System.out.print("Enter no. of rows: ");
         int n = s.nextInt();
  
-        for (int i = 0; i <= n; i++) {
+        for (int i = 1; i <= n; i++) {
             for (int j = (n - i); j >= 1; j--) {
                 System.out.print(" ");
             }
@@ -22,13 +22,12 @@ public class diamondNo {
             }
             System.out.println();
         }
-        for (int i = n; i > 0; i--) {
-            System.out.print(" ");
+        for (int i = n -1; i > 0; i--) {
             for (int j = (n - i); j >= 1; j--) {
                 System.out.print(" ");
             }
             
-            for (int j = 1; j < i; j++) {
+            for (int j = 1; j <= i; j++) {
                 if (j % 2 == 0) {
                     System.out.print("0 ");
                 }
