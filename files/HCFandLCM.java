c2801094c8c57ff20904937afd6ab0984d586e9f
rajdeep-paul16 <rajdeeppaul1614@gmail.com>
2022-09-08 21:51:22 +0530

    program to find HCF and LCM of two numbers

---
+++ files/HCFandLCM.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+
+public class HCFandLCM {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+
+        float b, lcm;
+
+        System.out.print("Enter the first number: ");
+        float n = s.nextFloat();
+
+        System.out.print("Enter the first number: ");
+        float m = s.nextFloat();
+
+        float product = n * m;
+
+        lcm = (n > m) ? n : m;
+        
+        while(lcm <= product) {
+            if (lcm % n == 0 && lcm % m == 0) {
+                
+                System.out.println("the LCM is " + lcm);
+                
+                b = product / lcm;
+                System.out.println("the HCF is " + b);
+                
+                break;
+            }
+            lcm++;
+        }
+        s.close();
+    }
+}
