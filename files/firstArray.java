ddfb742bb4557c328b5d1bd071535882b41825ab
rajdeep-paul16 <rajdeeppaul1614@gmail.com>
2022-09-16 19:15:54 +0530

    modified the code to ask the user for no. of elements they want to display

--- files/firstArray.java
+++ files/firstArray.java
@@ -4,9 +4,12 @@ public class firstArray {
     public static void main(String[] args) {
         Scanner s = new Scanner(System.in);
 
-        int[] arr = new int[10];
+        System.out.print("Enter number of elements you want to display: ");
+        int m = s.nextInt();
 
-        for (int i = 0; i < 10; i++) {
+        int[] arr = new int[m];
+
+        for (int i = 0; i < m; i++) {
             System.out.print("Enter the number: ");
             int n = s.nextInt();
 
@@ -14,7 +17,7 @@ public class firstArray {
         }
 
         System.out.println("The numbers entered are: ");
-        for (int j = 0; j < 10; j++) {
+        for (int j = 0; j < m; j++) {
             System.out.print(arr[j] + " ");
         }
         s.close();
