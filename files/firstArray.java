a76b2e18d1205fc30c8fc21d58a41738f151bb42
rajdeep-paul16 <rajdeeppaul1614@gmail.com>
2022-09-16 19:50:11 +0530

    asking user for a no. to check if it is in the array

--- files/firstArray.java
+++ files/firstArray.java
@@ -20,6 +20,23 @@ public class firstArray {
         for (int j = 0; j < m; j++) {
             System.out.print(arr[j] + " ");
         }
+        System.out.println(" ");
+
+        int found = 0;
+        System.out.print("Enter the number you want to check: ");
+        int k = s.nextInt();
+        for (int d = 0; d < m; d++) {
+            if (arr[d] == k) {
+                found = 1;
+            }
+        }
+
+        if (found == 1) {
+            System.out.print("Yes, the number you want to check is present");
+        }
+        else {
+            System.out.print("No, the number you want to check is not present");
+        }
         s.close();
     }
 }
